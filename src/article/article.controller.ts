3c570226597f157e507447a805bd6d2f3c0e7088
Raman Andreyuk <romaandreyuk@gmail.com>
2026-06-10 14:58:04 +0200

    create buildArticleResponse func articleResponseInterface for client response and create Slug from title + unique string

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -3,7 +3,8 @@ import { ArticleService } from '@app/article/article.service';
 import { AuthGuard } from '@app/user/guards/auth.guard';
 import { User } from '@app/user/decorators/user.decorator';
 import { UserEntity } from '@app/user/user.entity';
-import { CreateArticleDto } from '@app/article/dto/create.article.tdo';
+import { CreateArticleDto } from '@app/article/dto/create.article.dto';
+import { ArticleResponseInterface } from '@app/article/types/article.response.interface';
 
 @Controller('articles')
 export class ArticleController {
@@ -12,7 +13,8 @@ export class ArticleController {
 
   @Post()
   @UseGuards(AuthGuard)
-  async create(@User() currentUser: UserEntity, @Body('article') createArticleDto: CreateArticleDto ): Promise<any> {
-    return this.articleService.createArticle(currentUser, createArticleDto);
+  async create(@User() currentUser: UserEntity, @Body('article') createArticleDto: CreateArticleDto ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.createArticle(currentUser, createArticleDto);
+    return this.articleService.buildArticleResponse(article);
   }
 }
