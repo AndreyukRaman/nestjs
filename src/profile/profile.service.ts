bf1725338ccfc447bc209b6b22d84dd7705b399a
Raman Andreyuk <romaandreyuk@gmail.com>
2026-06-18 13:09:01 +0200

    @Post follow profile

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,10 +4,14 @@ import { ProfileResponseInterface } from '@app/profile/types/profileResponse.int
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@app/user/user.entity';
 import { Repository } from 'typeorm';
+import { FollowEntity } from '@app/profile/follow.entity';
 
 @Injectable()
 export class ProfileService {
-  constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity> ) {
+  constructor(
+    @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity) private readonly followRepository: Repository<FollowEntity>
+  ) {
   }
 async  getProfile(currentUserId:number, profileUsername: string): Promise<ProfileType>{
 const user = await this.userRepository.findOne({
@@ -21,6 +25,35 @@ const user = await this.userRepository.findOne({
   return {...user, following: false};
 
   }
+
+async followProfile(currentUserId:number, profileUsername: string): Promise<ProfileType>{
+  const user = await this.userRepository.findOne({
+    where: {
+      username: profileUsername
+    }
+  })
+  if(!user){
+    throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+  }
+
+  if(currentUserId === user.id){
+    throw new HttpException('Follower and following cant be equal', HttpStatus.BAD_REQUEST);
+  }
+
+const follow= await this.followRepository.findOne({where: {
+  followerId: currentUserId,
+    followingId: user.id,
+  }})
+
+  if(!follow){
+    const followToCreate = new FollowEntity()
+    followToCreate.followerId = currentUserId
+    followToCreate.followingId = user.id
+    await this.followRepository.save(followToCreate);
+  }
+
+  return{...user, following:true}
+}
 
 
   buildProfileResponse(profile:ProfileType): ProfileResponseInterface{
