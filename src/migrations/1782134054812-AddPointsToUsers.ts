822ecea93aceb7d5a895ee07442a80097e0abb10
Raman Andreyuk <romaandreyuk@gmail.com>
2026-06-22 15:57:48 +0200

    added default points into migration

--- src/migrations/1782134054812-AddPointsToUsers.ts
+++ src/migrations/1782134054812-AddPointsToUsers.ts
@@ -4,7 +4,7 @@ export class AddPointsToUsers1782134054812 implements MigrationInterface {
     name = 'AddPointsToUsers1782134054812'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "points" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "points" integer NOT NULL DEFAULT 10`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
